dcd770aa6469bbb02e5fa9200193bcfb799b0446
AJ NOURI <ajn.bin@gmail.com>
2015-01-26 04:24:06 +0100

    Delete synch.sh

--- synch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-scp *.py ajn@192.168.0.121:/home/ajn/coding/sdn/mininet-dc-br
-scp *.sh ajn@192.168.0.121:/home/ajn/coding/sdn/mininet-dc-br
-scp -r .git ajn@192.168.0.121:/home/ajn/coding/sdn/mininet-dc-br
